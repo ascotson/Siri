a9a4b076922b24fb1498cdb6d592b7dc7ac64984
ascotson <ascotson@gmail.com>
2016-01-13 19:15:20 -0700

    All tasks completed. Working fine. Added cors to overcome the problem with not recognizing the 'Access-Control-Allow-Origin'.

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ var express = require('express');
 
 var app = express();
 
+var bodyParser = require('body-parser');
+
+var cors = require('cors');
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
 app.listen(8887, function() {
   console.log('Listening on port 8887');
 });
@@ -14,7 +22,15 @@ function getRandomMessage() {
 }
 
 app.get('/', function(req, res) {
-  res.send(JSON.stringify({
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify({
     message: getRandomMessage()
   }));
 });
